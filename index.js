08aee96d5cc98c73e6be5d7d5fb9291a39a7f645
Chopperxx <ilua292010@gmail.com>
2021-09-26 01:06:05 +0300

    CU-1hwx5yh - [Пакет] Установка express.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('config')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/v1',  require('./routes/genre.route'))
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURL'))
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+
+// app.get('/', (req,res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
---
+++ routes/genre.route.js
@@ -0,0 +1,74 @@
+const {Router} = require('express')
+const Genre = require('../models/Genre')
+const router = Router()
+
+
+//  api/v1
+router.post('/genre', async (req, res) => {
+    try {
+        const { name } = req.body
+        const genreFind = await Genre.findOne({name})
+        
+        if (genreFind) {
+            return res.status(400).json({message: 'Такой жанр уже есть!'})
+        }
+
+        const genre = new Genre({name})
+
+        await genre.save()
+
+        res.status(201).json({message: 'Жанр создан!'})
+
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
+})
+router.put('/genre/:id', async (req,res) => {
+    try {
+        const {name} = req.body
+        const genre = await Genre.findByIdAndUpdate(req.params.id, {name}, {new: true})
+
+        res.status(200).json(genre)
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
+})
+router.get('/genre/:id', async (req,res) => {
+    try {
+        const genre  = await Genre.findById(req.params.id)
+
+        res.status (200).json(genre)
+    }catch (e) {
+        res.status(500).json({message: e.message})
+    }
+})
+
+router.delete('/genre/:id', async (req,res) => {
+    console.log(req.params.id)
+
+    try {
+        const deleteGenre = await Genre.findByIdAndDelete(req.params.id)
+
+        if (deleteGenre) {
+            res.status(200).send()
+        }
+
+    }catch (e) {
+
+    }
+
+})
+
+router.get('/genres', async (req,res) => {
+    try {
+        const genres = await Genre.find({})
+
+        res.status(200).json(genres)
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
+})
+
+
+
+module.exports = router
