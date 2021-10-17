2cc9fb6cdde5c6aa076ff3324e8624d79cf38064
Chopperxx <ilua292010@gmail.com>
2021-10-17 14:23:26 +0300

    CU-1my60by[COMPLETE] - POST - создание перевода (Сервер)

--- routes/translate.route.js
+++ routes/translate.route.js
@@ -1,8 +1,26 @@
 const { Router } = require('express')
+const { SAME_INSTANCE } = require('../helpers/messages')
 const { calculatedPage, makePagination } = require('../helpers/methods')
 const Translate = require('../models/Translate')
 const router = Router()
 
+router.post('/translate', async (req,res) => {
+    try {
+        const {name} = req.body
+
+        const uniqueTranslate = await Translate.findOne({name})
+
+        if (uniqueTranslate) return res.status(400).json({error: SAME_INSTANCE})
+
+        const translate = new Translate({name})
+
+        await translate.save()
+
+        res.status(200).json(translate);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
 
 router.get('/translate', async (req,res) => {
     try {
